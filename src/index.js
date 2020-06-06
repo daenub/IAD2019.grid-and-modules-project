fc503e8741386f90f2ffe1c5b8a13fd58a711e69
Dan Büschlen <danbueschlen@gmail.com>
2020-06-06 15:30:01 +0200

    chorus setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import "./scss/index.scss"
 import ScrollMagic from "ScrollMagic"
 import "ScrollMagic/scrollmagic/uncompressed/plugins/debug.addIndicators"
 
+const onResizeQueue = []
+
+window.addEventListener("resize", () => onResizeQueue.forEach(f => f()))
+
 document.addEventListener("DOMContentLoaded", () => {
   const controller = new ScrollMagic.Controller()
 
@@ -32,5 +36,26 @@ document.addEventListener("DOMContentLoaded", () => {
       .setClassToggle(bridgeEl, `scene-${s}`)
       .addIndicators({name: `scene ${s}`}) // add indicators (requires plugin)
   }))
-})
 
+  const chorus1Trigger = document.querySelector("#chorus-1")
+  let chorus1Height = chorus1Trigger.offsetHeight
+
+  const chorus1PinScene = new ScrollMagic.Scene({
+    triggerElement: chorus1Trigger,
+    duration: chorus1Height
+  })
+    .setPin("#chorus-1-pin", {pushFollowers: false})
+    .addIndicators({name: "Sticky"})
+    .addTo(controller)
+
+  onResizeQueue.push(() => console.log(chorus1PinScene.duration(chorus1Trigger.offsetHeight)))
+
+  const chorus1Scenes = [1,2,3,4,5,6,7,8,9]
+
+  controller.addScene(chorus1Scenes.map((s, i) => {
+    const el = chorus1Trigger.querySelector(`#always__sub--${s}`)
+    return new ScrollMagic.Scene({triggerElement: el})
+      .setClassToggle(el, "visible")
+      // .addIndicators({name: `Chorus 1 Always ${s}`}) // add indicators (requires plugin)
+  }))
+})
