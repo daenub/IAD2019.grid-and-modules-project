b83d19f2d226e90946b443bbf33d642c2077587d
Dan Büschlen <danbueschlen@gmail.com>
2020-06-06 09:04:24 +0200

    basic verse-2 structure

--- src/index.js
+++ src/index.js
@@ -7,13 +7,19 @@ document.addEventListener("DOMContentLoaded", () => {
   const controller = new ScrollMagic.Controller()
 
   const verse1Scenes = [1,2,3,4,5,6]
-  const verse1Duration = 50
 
   controller.addScene(verse1Scenes.map((s, i) => {
-    console.log((verse1Duration * i) + "%")
     return new ScrollMagic.Scene({triggerElement: `#verse-1-trigger-scene-${s}`})
       .setClassToggle("#verse-1", `scene-${s}`)
-      // .addIndicators({name: `scene ${s} (duration: ${verse1Duration * 0})`}) // add indicators (requires plugin)
+      .addIndicators({name: `scene ${s}`}) // add indicators (requires plugin)
+  }))
+
+  const verse2Scenes = [1,2,3,4,5,6,7,8]
+
+  controller.addScene(verse2Scenes.map((s, i) => {
+    return new ScrollMagic.Scene({triggerElement: `#verse-2-trigger-scene-${s}`})
+      .setClassToggle("#verse-2", `scene-${s}`)
+      .addIndicators({name: `scene ${s}`}) // add indicators (requires plugin)
   }))
 })
 
